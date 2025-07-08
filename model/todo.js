da79dc49a07d260612948ddc924f99385c36a8bc
Azad Hüseyin Kılıç <73038601+economicseeker@users.noreply.github.com>
2025-07-08 18:02:20 +0300

    Add DB error logging to model/todo.js for easier debugging

--- model/todo.js
+++ model/todo.js
@@ -1,17 +1,36 @@
 const pool = require('./database');
 
 // Create a new todo item
-const create = (description) =>
-  pool.query('INSERT INTO todo (description) VALUES ($1) RETURNING *', [
-    description,
-  ]);
+const create = async (description) => {
+  try {
+    return await pool.query('INSERT INTO todo (description) VALUES ($1) RETURNING *', [
+      description,
+    ]);
+  } catch (err) {
+    console.error('DB error in create:', err);
+    throw err;
+  }
+};
 
 // Get all todo items
-const get = () => pool.query('SELECT * FROM todo');
+const get = async () => {
+  try {
+    return await pool.query('SELECT * FROM todo');
+  } catch (err) {
+    console.error('DB error in get:', err);
+    throw err;
+  }
+};
 
 // Remove a todo item by id
-const remove = (id) =>
-  pool.query('DELETE FROM todo WHERE todo_id = $1', [id]);
+const remove = async (id) => {
+  try {
+    return await pool.query('DELETE FROM todo WHERE todo_id = $1', [id]);
+  } catch (err) {
+    console.error('DB error in remove:', err);
+    throw err;
+  }
+};
 
 module.exports = {
   create,
